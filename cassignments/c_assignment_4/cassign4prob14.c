f2e17b3ca5e56d18331b64fa0a3a9688fcd1ec69
ShiavniKaripe <karipe.shivani1898@gmail.com>
2019-02-11 19:18:06 +0530

    added c assignment 5 problem 1

---
+++ cassignments/c_assignment_4/cassign4prob14.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+void printRotatedString(char str[]) 
+{ 
+    int len = strlen(str); 
+  
+    // Generate all rotations one by one and print 
+    char temp[len]; 
+    for (int i = 0; i < len; i++) 
+    { 
+        int j = i;  // Current index in str 
+        int k = 0;  // Current index in temp 
+  
+        // Copying the second part from the point 
+        // of rotation. 
+        while (str[j] != '\0') 
+        { 
+            temp[k] = str[j]; 
+            k++; 
+            j++; 
+        } 
+  
+        // Copying the first part from the point 
+        // of rotation. 
+        j = 0; 
+        while (j < i) 
+        { 
+            temp[k] = str[j]; 
+            j++; 
+            k++; 
+        } 
+  
+        printf("%s\n", temp); 
+    } 
+} 
+  
+ 
+int main() 
+{ 
+    char str[100];
+	printf("enter a string");
+gets(str); 
+    printRotatedString(str); 
+    return 0; 
+} 
