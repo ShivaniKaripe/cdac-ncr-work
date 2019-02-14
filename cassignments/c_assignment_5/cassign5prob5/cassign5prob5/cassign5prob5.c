0cbfe8daefc4002c2ded7150e135931532161c29
“ShivaniKaripe” <karipe.shivani1898@gmail.com>
2019-02-15 03:11:09 +0530

    c assignmet 4 prob 6

--- cassignments/c_assignment_5/cassign5prob5/cassign5prob5/cassign5prob5.c
+++ cassignments/c_assignment_5/cassign5prob5/cassign5prob5/cassign5prob5.c
@@ -6,10 +6,13 @@ int main()
 	scanf_s("%d %d %d", &day1, &mon1, &year1);
 	printf_s("enter the second date in the order of day, month,year\n ");
 	scanf_s("%d %d %d", &day2, &mon2, &year2);
-	if (mon1 > 12 || mon2 > 12 || day1 > 31 || day2 > 31)
-	{
-		printf_s("Invalid input");
-		return -1;
-	}
+	 
+	int days,temp,n;
+	 
 
+
+	 
+	system("pause");
+	
+    
 }
