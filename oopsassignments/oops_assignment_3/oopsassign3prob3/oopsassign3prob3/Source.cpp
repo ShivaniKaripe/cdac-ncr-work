17df08979039965d1bfcc9f9d04d914c6ea79df7
“ShivaniKaripe” <karipe.shivani1898@gmail.com>
2019-03-17 18:22:33 +0530

    added oops assignment 3 problem 3

--- oopsassignments/oops_assignment_3/oopsassign3prob3/oopsassign3prob3/Source.cpp
+++ oopsassignments/oops_assignment_3/oopsassign3prob3/oopsassign3prob3/Source.cpp
@@ -37,7 +37,7 @@ public:
 	}
 	void display_area()
 	{
-		cout << "the area of the triangle is " << b*l << endl;
+		cout << "the area of the Rectangle is " << b*l << endl;
 	}
 };
 class Square : public Shape
@@ -50,29 +50,65 @@ public:
 	}
 	void display_area()
 	{
-		cout << "the area of the triangle is " << a*a << endl;
+		cout << "the area of the square is " << a*a << endl;
 	}
 };
 int main()
 {
+	char choice[10]="";
 	int a, b;
-	Shape s;
-	cout << "enter the breadth value" << endl;
-	cin >> a;
-	cout << "enter the height value" << endl;
-	cin >> b;
-	Triangle t(a, b);
-	t.display_area();
-	cout << "enter the breadth value" << endl;
-	cin >> a;
-	cout << "enter the lenght value" << endl;
-	cin >> b;
-	Rectangle r(a, b);
-	r.display_area();
-	cout << "enter the side value" << endl;
-	cin >> a;
-	 
-	Square sq(a);
-	sq.display_area();
+	Shape *s;
+	while (*choice != '4')
+	{
+		cout << "choose any of the following" << endl;
+		cout << "find the area of triangle" << endl;
+		cout << "find the area of rectangle" << endl;
+		cout << "find the area of square" << endl;
+		cout << "exit" << endl;
+		cout << "enter your choice" << endl;
+		cin >> choice;
+		if (*choice != '4')
+		{
+			switch (*choice)
+			{
+			case '1':
+			{
+				cout << "enter the breadth value" << endl;
+				cin >> a;
+				cout << "enter the height value" << endl;
+				cin >> b;
+				Triangle t(a, b);
+				s = &t;
+				s->display_area();
+				break;
+			}
+			case '2':
+			{
+				cout << "enter the breadth value" << endl;
+				cin >> a;
+				cout << "enter the lenght value" << endl;
+				cin >> b;
+				Rectangle r(a, b);
+				s = &r;
+				s->display_area();
+				break;
+			}
+			case '3':
+			{
+				cout << "enter the side value" << endl;
+				cin >> a;
+				Square sq(a);
+				s = &sq;
+				s->display_area();
+				break;
+			}
+			default:
+			{	cout << "enter a valid choice" << endl;
+			break;
+			}
+			}
+		}
+	} 
+	
 	system("pause");
 }
